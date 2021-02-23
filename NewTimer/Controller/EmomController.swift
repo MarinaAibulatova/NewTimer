79a8f542f2606fcc86b68731c94c179c624a59ec
Marina Aibulatova <marinka-dgerry@yandex.ru>
2021-02-23 17:38:08 +0300

    add emom view

---
+++ NewTimer/Controller/EmomController.swift
@@ -0,0 +1,16 @@
+//
+//  EmomController.swift
+//  NewTimer
+//
+//  Created by Марина Айбулатова on 23.02.2021.
+//
+
+import Foundation
+import UIKit
+
+class EmomController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
